dc737e82acb143b2e772a6e99e24a59bce7dd91f
Thiago Souza Oliveira <77512009+ThePsychoBard@users.noreply.github.com>
2021-09-23 20:08:14 -0300

    questâo 8 modificada
    todas as minhas alterações estão aqui no main.

--- Exemplo_08/main.c
+++ Exemplo_08/main.c
@@ -2,62 +2,136 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "ferramentas.h"
 
-int main(void)
+#define TRUE 1
+#define FALSE 0
+
+
+typedef struct
 {
-    int tamLista, capLista;
-    /* capLista - Capacidade da lista.  Limite máximo de elememtos
-        que podem ser aramzenados na lista. Pode ser entendido como
-        o tamanho do array
-    */
-    capLista = LSIZE;
-    /* tamLista - Tamanho da lista
-        * tamLista == 0: Lista vazia
-        * tamLista == capLista: Lista cheia
-    */
-    tamLista = 0;
-    // Uma lista como uma array alocado estaticamente
-    TAluno lista[LSIZE];
+    int numMatricula; //chave com 9 dígitos (INT_MAX	+2147483647)
+    char nome[100];   // nome com até 99 caracteres
+    char email[100];  // eMail com até 99 caracteres
+} TAluno;
 
-    printf("Testando Listas Lineares Sequenciais\n");
 
-    /* Vamos adicionar 3 alunos na lsita
-        Nome                    Matrícula   EMail
-        Regrano Guedes Maia     201913425   regguemai@uesc.br   
-        Mengano Martins Pereira 201913245   menmarper@uesc.br
-        Fulano Silva Oliveira   201912345   fulsiloli@uesc.br
-    */
+typedef struct{
+	TAluno* lista;
+	int cap;
+	int tam;
+    int eOrd;
+} TListAlunos;
 
-    // Adicionando o primeiro elemento na lista
-    TAluno aluno;
-    aluno.numMatricula = 201913425;
-    strcpy(aluno.nome, "Regrano Guedes Maia");
-    strcpy(aluno.email, "regguemai@uesc.br");
-    if(incLisAlunoOrd(aluno, lista, &tamLista) == FALSE)
-        printf("Aluno %d já está na lista\n", aluno.numMatricula);
-    // Adicionando um elemento na lista com chave repetida
-    aluno.numMatricula = 201913425;
-    strcpy(aluno.nome, "Mengano Martins Pereira");
-    strcpy(aluno.email, "menmarper@uesc.br");
-    if(incLisAlunoOrd(aluno, lista, &tamLista) == FALSE)
-        printf("Aluno %d já está na lista\n", aluno.numMatricula);
-    // Adicionando um novo elemento na lista
-    aluno.numMatricula = 201913245;
-    strcpy(aluno.nome, "Mengano Martins Pereira");
-    strcpy(aluno.email, "menmarper@uesc.br");
-    if(incLisAlunoOrd(aluno, lista, &tamLista) == FALSE)
-        printf("Aluno %d já está na lista\n", aluno.numMatricula);
-    // Adicionando um novo elemento na lista
-    aluno.numMatricula = 201912345;
-    strcpy(aluno.nome, "Fulano Silva Oliveira");
-    strcpy(aluno.email, "fulsiloli@uesc.br");
-    if(incLisAlunoOrd(aluno, lista, &tamLista) == FALSE)
-        printf("Aluno %d já está na lista\n", aluno.numMatricula);
 
-    // Imprimindo a lista
-    printLisAluno(lista, tamLista);
+int buscaLisAlunoOrd(TAluno lista[], int chave);
+int incListAlunoOrd(TAluno aluno, TAluno lista[], int *tam);
+void trocaAluno(TAluno *alunoA, TAluno *alunoB);
+void printLisAluno(TAluno lista[], int *tam);
+int remLisAlunoOrd(TAluno aluno, TAluno lista[], int *tam);
+
+
+int main()
+{
+   	TAluno aluno;
+	TAluno* lista;
+	lista = (TAluno*) malloc(5*sizeof(TAluno));
+
+    int tamLista =  0;
+    printf("Testando inclusão em lista ordenada\n");
+    aluno.numMatricula = 1;
+    strcpy(aluno.nome, "Fulano pereira");
+    strcpy(aluno.email,"fulano_pereira@uesc.br");
+	if(incListAlunoOrd(aluno, lista, &tamLista))
+        printf("incluiu\n");
+
+    aluno.numMatricula = 3;
+    strcpy(aluno.nome, "ciclano conceicao");
+    strcpy(aluno.email,"fulaninho_conceicao@uesc.br");
+	if(incListAlunoOrd(aluno, lista, &tamLista))
+        printf("incluiu\n");
+
+	aluno.numMatricula = 2;
+    strcpy(aluno.nome, "beltrano martins");
+    strcpy(aluno.email,"beltranim_martim@uesc.br");
+	if(incListAlunoOrd(aluno, lista, &tamLista))
+        printf("incluiu\n");
+
+	aluno.numMatricula = 0;
+    strcpy(aluno.nome, "teste martins");
+    strcpy(aluno.email,"teste_martim@uesc.br");
+	if(incListAlunoOrd(aluno, lista, &tamLista))
+        printf("incluiu\n");
+
+    printLisAluno(lista, &tamLista);
+
+    printf("Testando remocao ordenada\n");
+    if(remLisAlunoOrd(aluno, lista, &tamLista))
+       printf("Remocao funcionou!");
+    else
+        printf("Remocao n funcionou!");
+
+    printLisAluno(lista, &tamLista);
 
     return 0;
 }
 
+
+//retorna o indice da posição que encontrou ou do primeiro elemento maior que a chave
+int buscaLisAlunoOrd(TAluno lista[], int chave)
+{
+    int i = 0;
+    while (lista[i].numMatricula < chave){
+        i++;
+    }
+    return i;
+}
+
+void trocaAluno(TAluno *alunoA, TAluno *alunoB){
+	alunoB->numMatricula = alunoA->numMatricula;
+	strcpy(alunoB->nome, alunoA->nome);
+	strcpy(alunoB->email, alunoA->email);
+}
+
+//inclue um aluno em um array de alunos ordenado
+int incListAlunoOrd(TAluno aluno, TAluno lista[], int *tam)
+{
+    int pos = buscaLisAlunoOrd(lista, aluno.numMatricula);
+    if (pos == *tam){
+        trocaAluno(&aluno, &lista[*tam] );
+        *tam += 1;
+        return TRUE;
+    }else if(lista[pos].numMatricula != aluno.numMatricula){
+        for(int i = *tam; i > pos; i--){
+            trocaAluno(&lista[i-1], &lista[i] );
+        }
+        trocaAluno(&aluno, &lista[pos] );
+        *tam += 1;
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int remLisAlunoOrd(TAluno aluno, TAluno lista[], int *tam){
+    if(*tam > 0){
+        int pos = buscaLisAlunoOrd(lista,aluno.numMatricula);
+        if(lista[pos].numMatricula == aluno.numMatricula){
+            *tam -= 1;
+            for(int i =pos; i < *tam; i++)
+                trocaAluno(&lista[i+1], &lista[i]);
+            return TRUE;
+        }
+    }
+    return FALSE;
+}
+
+void printLisAluno(TAluno lista[], int *tam)
+{
+	printf("[ \n ");
+	for (int i = 0; i < *tam; i++)
+	{
+		printf("%d, ", lista[i].numMatricula);
+		printf("%s, ", lista[i].nome);
+		printf("%s;\n ", lista[i].email);
+	}
+	printf(" ]\n");
+}
